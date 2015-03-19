27037942364ca3b5091f09997ef9ac5ba83611c6
Stefan Hesse <stefan.hesse@tu-dortmund.de>
2015-03-19 10:45:28 +0100

    fixing autload

--- config/module.config.php
+++ config/module.config.php
@@ -1,18 +1,7 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return array(
     'service_manager' => array(
         'abstract_factories' => array(
-            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-            'Zend\Log\LoggerAbstractServiceFactory',
-	        'Application\Factory\ApigilityFormGeneratorFactory'
         ),
         'aliases' => array(
 

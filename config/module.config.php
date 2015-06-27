580e810e0969383b7f45c633b74173fb6dd7a305
Stefan Hesse <stefan.hesse@tu-dortmund.de>
2015-06-27 19:01:30 +0200

    update config for heler

--- config/module.config.php
+++ config/module.config.php
@@ -1,26 +1,27 @@
 <?php
 return array(
-    'service_manager' => array(
-        'abstract_factories' => array(
-	        'Reinoldus\Doctrine\Factory\EntityServiceAbstractFactory'
-        ),
-        'aliases' => array(
+	'service_manager' => array(
+		'abstract_factories' => array(
+			'Reinoldus\Doctrine\Factory\EntityServiceAbstractFactory'
+		),
+		'aliases' => array(
 
-        ),
-	    'factories' => array(
-		    'Reinoldus\Doctrine\Mapper\ApigilityDoctrineMapper' => 'Reinoldus\Doctrine\Factory\ApigilityDoctrineMapperFactory',
-		    'Reinoldus\ZfcUser\Mapper\ZfcUserOauth2Mapper' => function($sl) {
+		),
+		'factories' => array(
+			'Reinoldus\Doctrine\Mapper\ApigilityDoctrineMapper' => 'Reinoldus\Doctrine\Factory\ApigilityDoctrineMapperFactory',
+			'Reinoldus\ZfcUser\Mapper\ZfcUserOauth2Mapper' => function($sl) {
 				return new Reinoldus\ZfcUser\Mapper\ZfcUserOauth2Mapper($sl);
-		    },
-		    'Reinoldus\Apigility\Service\FormGenerator' => 'Reinoldus\Apigility\Factory\FormGenerator'
-	    ),
-	    'invokables' => array(
-		    'Reinoldus\Doctrine\Mapper\ApigilityDoctrineToArray' => 'Reinoldus\Doctrine\Mapper\ApigilityDoctrineToArray'
-	    )
-    ),
+			},
+			'Reinoldus\Apigility\Service\FormGenerator' => 'Reinoldus\Apigility\Factory\FormGenerator'
+		),
+		'invokables' => array(
+			'Reinoldus\Doctrine\Mapper\ApigilityDoctrineToArray' => 'Reinoldus\Doctrine\Mapper\ApigilityDoctrineToArray'
+		)
+	),
 	'validators' => array(
 		'factories' => array(
-			'Reinoldus\Doctrine\Validator\UniqueValue' => 'Reinoldus\Doctrine\Validator\UniqueValueFactory'
+			'Reinoldus\Doctrine\Validator\UniqueValue' => 'Reinoldus\Doctrine\Validator\UniqueValueFactory',
+			'Reinoldus\Doctrine\Validator\NotEmptyArrayAndValue' => 'Reinoldus\Doctrine\Validator\NotEmptyArrayAndValueFactory'
 		),
 	),
 	'validator_metadata' => array(
@@ -28,6 +29,19 @@ return array(
 			'service' => 'string',
 			'fields' => 'string',
 		),
+		'Reinoldus\Doctrine\Validator\NotEmptyArrayAndValue' => array(
+			'message' => 'string',
+		)
+	),
+	'view_helpers' => array(
+		'factories'=> array(
+			'routeCheck' => function($pluginManager) {
+				/**
+				 * @var \Zend\View\HelperPluginManager $pluginManager
+				 */
+				return new Reinoldus\ViewHelper\RouteCheck($pluginManager->getServiceLocator()->get('Application'));
+			}
+		)
 	),
 	'view_manager' => array(
 		'template_map' => array(
